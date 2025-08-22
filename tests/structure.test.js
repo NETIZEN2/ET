1c16a9c06f1dba23d1f71ab5c90ab103639b1075
NETIZEN2 <jacob.tyler.wiremu@hotmail.com>
2025-08-22 21:25:31 +1000

    Delete tests directory

--- tests/structure.test.js
+++
@@ -1,9 +0,0 @@
-import { strict as assert } from 'node:assert';
-import { readFileSync } from 'node:fs';
-
-const html = readFileSync('./index.html','utf8');
-const match = html.match(/<div class="segmented"[\s\S]*?<\/div>/);
-assert.ok(match, 'segmented control exists');
-const buttons = match[0].match(/<button/g) || [];
-assert.equal(buttons.length,3);
-console.log('structure.test passed');
